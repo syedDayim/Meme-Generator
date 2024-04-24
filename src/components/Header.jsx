4a53b6272cc5633af6aec3eb7a7b2fb994559bf5
Syed Dayim Shah <90561337+syedDayim@users.noreply.github.com>
2024-04-24 16:25:25 +0530

    bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className='flex justify-between items-center px-6 w-[auto] h-[65px] bg-gradient-to-r from-[#672280] to-[#A626D3]' >
       <div className='flex justify-center items-center gap-[7px]'>
         <img className='w-[36.31px] h-[26.22]' src="./firebaseLogo.png" alt="" />
-        <h1 className='font-bold text-xl text-white'>Shuraa Digital</h1>
+        <h1 className='font-bold text-xl text-white'>Meme Generator</h1>
       </div>
       <p className='font-medium text-xs text-white'>React Project - 3</p>
     </header>
